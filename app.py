9024cb02fe739e89b516c0a853a3186955f432a0
mohammed iliyas <125146066+Iliyasiliyas@users.noreply.github.com>
2026-03-17 15:47:30 +0530

    Change MAIL_PASSWORD in Flask-Mail configuration
    Updated the MAIL_PASSWORD for Flask-Mail configuration.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'azhiyaroil@gmail.com'  # Replace with your Gmail address
-app.config['MAIL_PASSWORD'] = 'lpyf eais tgdb onyp'  # Replace with your generated App Password
+app.config['MAIL_PASSWORD'] = 'esax cyrx goyk mazk'  # Replace with your generated App Password
 app.config['MAIL_DEFAULT_SENDER'] = 'azhiyaroil@gmail.com'  # Replace with your Gmail address
 
 mail = Mail(app)
